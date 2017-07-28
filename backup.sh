088b00194adbd11b1724f570db4bcf58c3ef15ef
Ben Reichert <ben@tozny.com>
2017-07-28 14:29:02 -0700

    Need to be able to edit the tarball variable

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
 # Get timestamp
 : ${BACKUP_SUFFIX:=.$(date +"%Y-%m-%d-%H-%M-%S")}
-readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
+tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
 
 # Create a gzip compressed tarball with the volume(s)
 tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
